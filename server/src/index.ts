e9eaec2f48b3e9b1cb77dbbb0a040368fa777e82
shashank <shashank_@outlook.in>
2025-07-21 19:31:13 +0530

    removed prisma client gerated folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import identifyRouter from './routers/identifyRouter';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
